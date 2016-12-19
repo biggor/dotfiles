3a367cdd85c6d0c59c9cf6f74eb8cabde0506d88
biggor <bi990r@gmail.com>
2016-12-19 20:29:10 +0000

    install.sh update

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 rm ~/.vim*
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
-# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
